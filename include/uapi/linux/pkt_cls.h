094abcef2830e3afe622c1d4a1832aa8e04bceea
Willem de Bruijn <willemb@google.com>
2017-01-07 17:06:35 -0500

    net-tc: extract skip classify bit from tc_verd
    Packets sent by the IFB device skip subsequent tc classification.
    A single bit governs this state. Move it out of tc_verd in
    anticipation of removing that __u16 completely.
    
    The new bitfield tc_skip_classify temporarily uses one bit of a
    hole, until tc_verd is removed completely in a follow-up patch.
    
    Remove the bit hole comment. It could be 2, 3, 4 or 5 bits long.
    With that many options, little value in documenting it.
    
    Introduce a helper function to deduplicate the logic in the two
    sites that check this bit.
    
    The field tc_skip_classify is set only in IFB on skbs cloned in
    act_mirred, so original packet sources do not have to clear the
    bit when reusing packets (notably, pktgen and octeon).
    
    Change-Id: Ide8f66327e77a63afb6a3cba216dc0ae2d84bf41
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit e7246e122aaa99ebbb8ad7da80f35a20577bd8af)

--- include/uapi/linux/pkt_cls.h
+++ include/uapi/linux/pkt_cls.h
@@ -29,8 +29,6 @@ bit 6,7: Where this packet was last seen
 1: on the Ingress
 2: on the Egress
 
-bit 8: when set --> Request not to classify on ingress. 
-
 bits 9,10,11: redirect counter -  redirect TTL. Loop avoidance
 
  *
@@ -44,10 +42,6 @@ bits 9,10,11: redirect counter -  redirect TTL. Loop avoidance
 #define AT_STACK	0x0
 #define AT_INGRESS	0x1
 #define AT_EGRESS	0x2
-
-#define TC_NCLS          _TC_MAKEMASK1(8)
-#define SET_TC_NCLS(v)   ( TC_NCLS | (v & ~TC_NCLS))
-#define CLR_TC_NCLS(v)   ( v & ~TC_NCLS)
 
 #define S_TC_AT          _TC_MAKE32(12)
 #define M_TC_AT          _TC_MAKEMASK(2,S_TC_AT)
