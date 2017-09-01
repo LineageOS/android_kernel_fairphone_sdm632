3acaf409c0d40895123553eb1d7d067f6874286e
David Ahern <dsahern@gmail.com>
2017-09-01 08:18:07 -0700

    bpf: Collapse offset checks in sock_filter_is_valid_access
    Make sock_filter_is_valid_access consistent with other is_valid_access
    helpers.
    
    Requested-by: Daniel Borkmann <daniel@iogearbox.net>
    Change-Id: I82bff3edf949b03bdde0c6116bcb1dce484fbe79
    Signed-off-by: David Ahern <dsahern@gmail.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 9df59055ed8e2f817149e786b2a52bc17832d84e)

--- net/core/filter.c
+++ net/core/filter.c
@@ -3485,9 +3485,7 @@ static bool sock_filter_is_valid_access(int off, int size,
 	if (type == BPF_WRITE) {
 		switch (off) {
 		case offsetof(struct bpf_sock, bound_dev_if):
-			break;
 		case offsetof(struct bpf_sock, mark):
-			break;
 		case offsetof(struct bpf_sock, priority):
 			break;
 		default:
