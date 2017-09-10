1c5b4d31ad29f092612e6bdc628aaff352f071e2
Jesper Dangaard Brouer <brouer@redhat.com>
2017-09-10 09:47:02 +0200

    xdp: implement xdp_redirect_map for generic XDP
    Using bpf_redirect_map is allowed for generic XDP programs, but the
    appropriate map lookup was never performed in xdp_do_generic_redirect().
    
    Instead the map-index is directly used as the ifindex.  For the
    xdp_redirect_map sample in SKB-mode '-S', this resulted in trying
    sending on ifindex 0 which isn't valid, resulting in getting SKB
    packets dropped.  Thus, the reported performance numbers are wrong in
    commit 24251c264798 ("samples/bpf: add option for native and skb mode
    for redirect apps") for the 'xdp_redirect_map -S' case.
    
    Before commit 109980b894e9 ("bpf: don't select potentially stale
    ri->map from buggy xdp progs") it could crash the kernel.  Like this
    commit also check that the map_owner owner is correct before
    dereferencing the map pointer.  But make sure that this API misusage
    can be caught by a tracepoint. Thus, allowing userspace via
    tracepoints to detect misbehaving bpf_progs.
    
    Fixes: 6103aa96ec07 ("net: implement XDP_REDIRECT for xdp generic")
    Fixes: 24251c264798 ("samples/bpf: add option for native and skb mode for redirect apps")
    Change-Id: Ied07d7cc3166ed5145e6d04b5578c0ce130994d2
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 96c5508e3012ed0984ab93821d64ac1ff3279c09)

--- net/core/filter.c
+++ net/core/filter.c
@@ -2536,21 +2536,19 @@ static int xdp_do_redirect_map(struct net_device *dev, struct xdp_buff *xdp,
 	struct redirect_info *ri = this_cpu_ptr(&redirect_info);
 	unsigned long map_owner = ri->map_owner;
 	struct bpf_map *map = ri->map;
+	struct net_device *fwd = NULL;
 	u32 index = ri->ifindex;
-	struct net_device *fwd;
 	int err;
 
 	ri->ifindex = 0;
 	ri->map = NULL;
 	ri->map_owner = 0;
 
-	/* This is really only caused by a deliberately crappy
-	 * BPF program, normally we would never hit that case,
-	 * so no need to inform someone via tracepoints either,
-	 * just bail out.
-	 */
-	if (unlikely(xdp_map_invalid(xdp_prog, map_owner)))
-		return -EINVAL;
+	if (unlikely(xdp_map_invalid(xdp_prog, map_owner))) {
+		err = -EFAULT;
+		map = NULL;
+		goto err;
+	}
 
 	fwd = __dev_map_lookup_elem(map, index);
 	if (!fwd) {
@@ -2607,13 +2605,27 @@ int xdp_do_generic_redirect(struct net_device *dev, struct sk_buff *skb,
 			    struct bpf_prog *xdp_prog)
 {
 	struct redirect_info *ri = this_cpu_ptr(&redirect_info);
+	unsigned long map_owner = ri->map_owner;
+	struct bpf_map *map = ri->map;
+	struct net_device *fwd = NULL;
 	u32 index = ri->ifindex;
-	struct net_device *fwd;
 	unsigned int len;
 	int err = 0;
 
-	fwd = dev_get_by_index_rcu(dev_net(dev), index);
 	ri->ifindex = 0;
+	ri->map = NULL;
+	ri->map_owner = 0;
+
+	if (map) {
+		if (unlikely(xdp_map_invalid(xdp_prog, map_owner))) {
+			err = -EFAULT;
+			map = NULL;
+			goto err;
+		}
+		fwd = __dev_map_lookup_elem(map, index);
+	} else {
+		fwd = dev_get_by_index_rcu(dev_net(dev), index);
+	}
 	if (unlikely(!fwd)) {
 		err = -EINVAL;
 		goto err;
@@ -2631,10 +2643,12 @@ int xdp_do_generic_redirect(struct net_device *dev, struct sk_buff *skb,
 	}
 
 	skb->dev = fwd;
-	_trace_xdp_redirect(dev, xdp_prog, index);
+	map ? _trace_xdp_redirect_map(dev, xdp_prog, fwd, map, index)
+		: _trace_xdp_redirect(dev, xdp_prog, index);
 	return 0;
 err:
-	_trace_xdp_redirect_err(dev, xdp_prog, index, err);
+	map ? _trace_xdp_redirect_map_err(dev, xdp_prog, fwd, map, index, err)
+		: _trace_xdp_redirect_err(dev, xdp_prog, index, err);
 	return err;
 }
 EXPORT_SYMBOL_GPL(xdp_do_generic_redirect);
