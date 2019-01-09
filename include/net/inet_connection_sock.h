20ac94ff4e58a7cb6c500543c2b561163e8782b3
quakentsai(58374) <quakentsai@arimacomm.com.tw>
2019-01-09 10:20:26 +0800

    CTS fail testCreateServerSocketWithPortNoBacklog
    CTS fail case :
    CtsLibcoreTestCases
    libcore.javax.net.ServerSocketFactoryTest#testCreateServerSocketWithPortNoBacklog
    
    patch from :
    https://android-review.googlesource.com/c/platform/libcore/+/860771
    
    Defect:
    N/A
    
    Root cause:
    N/A
    
    How to fix:
    N/A
    
    Change-Id: I7f42e6f1835efe8e23707bc94c6574d8567b83fb

--- include/net/inet_connection_sock.h
+++ include/net/inet_connection_sock.h
@@ -289,10 +289,14 @@ static inline int inet_csk_reqsk_queue_len(const struct sock *sk)
 	return reqsk_queue_len(&inet_csk(sk)->icsk_accept_queue);
 }
 
+//2019 01 09-Quaken > fix CTS fail testCreateServerSocketWithPortNoBacklog
+/*
 static inline int inet_csk_reqsk_queue_young(const struct sock *sk)
 {
 	return reqsk_queue_len_young(&inet_csk(sk)->icsk_accept_queue);
 }
+*/
+//2019 01 09-Quaken < fix CTS fail testCreateServerSocketWithPortNoBacklog
 
 static inline int inet_csk_reqsk_queue_is_full(const struct sock *sk)
 {
