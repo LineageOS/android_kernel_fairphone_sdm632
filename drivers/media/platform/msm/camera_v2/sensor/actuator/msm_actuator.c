096e355acfd8a7e6d0a07f2a20d1eda94734bc16
frankcheng <frankcheng@arimacomm.com.tw>
2020-05-09 20:59:58 +0800

    Add Camera drivers for FP3
    Issue: FP3-A11#229
    Change-Id: Iecba04814a03123628c4a634d59834c395a5dd72

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -31,6 +31,9 @@ DEFINE_MSM_MUTEX(msm_actuator_mutex);
 #define PARK_LENS_SMALL_STEP 3
 #define MAX_QVALUE 4096
 
+extern int nActuatorAK7374;
+extern int nActuatorDW9800;
+
 static struct v4l2_file_operations msm_actuator_v4l2_subdev_fops;
 static int32_t msm_actuator_power_up(struct msm_actuator_ctrl_t *a_ctrl);
 static int32_t msm_actuator_power_down(struct msm_actuator_ctrl_t *a_ctrl);
@@ -118,7 +121,8 @@ static void msm_actuator_parse_i2c_params(struct msm_actuator_ctrl_t *a_ctrl,
 				write_arr[i].data_shift) |
 				((hw_dword & write_arr[i].hw_mask) >>
 				write_arr[i].hw_shift);
-
+				if (nActuatorAK7374 == 0 && nActuatorDW9800 == 0)
+					value = abs(1023-value);
 			if (write_arr[i].reg_addr != 0xFFFF) {
 				i2c_byte1 = write_arr[i].reg_addr;
 				i2c_byte2 = value;
