edd141218a1f1f38e81a127c1e0bf5025db0b699
Bharath <bharath@teamb58.org>
2022-10-21 09:46:47 +0530

    FP3: Merge branch 'int/13/fp3' into lineage-19.1
    * int/13/fp3: (208 commits)
      proc: Replace skip_initramfs to androidboot.force_normal_boot
      init: adjustments for retrofitted dynamic partitions
      arm64: dts: qcom: Nuke early mount
      defconfig: Disable CONFIG_DEBUG_INFO
      Linux 4.9.330
      ext4: make directory inode spreading reflect flexbg size
      Drivers: hv: Never allocate anything besides framebuffer from framebuffer memory region
      s390/dasd: fix Oops in dasd_alias_get_start_dev due to missing pavgroup
      serial: tegra: Use uart_xmit_advance(), fixes icount.tx accounting
      serial: Create uart_xmit_advance()
      net: sunhme: Fix packet reception for len < RX_COPY_THRESHOLD
      perf kcore_copy: Do not check /proc/modules is unchanged
      can: gs_usb: gs_can_open(): fix race dev->can.state condition
      MIPS: lantiq: export clk_get_io() for lantiq_wdt.ko
      net: team: Unsync device addresses on ndo_stop
      ipvlan: Fix out-of-bound bugs caused by unset skb->mac_header
      netfilter: nf_conntrack_irc: Tighten matching on DCC message
      netfilter: nf_conntrack_sip: fix ct_sip_walk_headers
      mm/slub: fix to return errno if kmalloc() fails
      ALSA: hda: add Intel 5 Series / 3400 PCI DID
      ...
    
    Change-Id: I5995d9608c82bfe0636b84526aaca19ef596511e

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,10 +2,13 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
+
+static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
@@ -21,8 +24,30 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+#define DYN_PART_CMDLINE_FIND "skip_initramfs"
+#define DYN_PART_CMDLINE_REPLACE "androidboot.force_normal_boot=1"
+
 static int __init proc_cmdline_init(void)
 {
+	size_t tail_len;
+	char *offset_addr;
+	char *cmd = new_command_line;
+
+	strcpy(cmd, saved_command_line);
+
+	// Find offset of find string
+	offset_addr = strstr(cmd, DYN_PART_CMDLINE_FIND);
+	if (offset_addr) {
+		tail_len = strlen(offset_addr + strlen(DYN_PART_CMDLINE_FIND));
+
+		// Move tail back so we have space for replace string
+		memmove(offset_addr + strlen(DYN_PART_CMDLINE_REPLACE),
+				offset_addr + strlen(DYN_PART_CMDLINE_FIND),
+				tail_len + 1);
+		// Copy replacement into the newly empty space
+		memcpy(offset_addr, DYN_PART_CMDLINE_REPLACE, strlen(DYN_PART_CMDLINE_REPLACE));
+	}
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
