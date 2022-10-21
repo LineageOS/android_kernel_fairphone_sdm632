28549c55a1b134156c3aeca28430e8f5e5e14b48
Bharath <bharath@teamb58.org>
2022-10-21 09:56:48 +0530

    FP3: Drop changes related to retrofit dynamic partition
    Since we currently don't require this.
    
    Change-Id: I9e06c581e578f0e56e6a10aa4ca3ee2d22eeed4e

--- init/initramfs.c
+++ init/initramfs.c
@@ -609,7 +609,6 @@ static void __init clean_rootfs(void)
 
 static int __initdata do_skip_initramfs;
 
-#if 0
 static int __init skip_initramfs_param(char *str)
 {
 	if (*str)
@@ -618,7 +617,6 @@ static int __init skip_initramfs_param(char *str)
 	return 1;
 }
 __setup("skip_initramfs", skip_initramfs_param);
-#endif
 
 static int __init populate_rootfs(void)
 {
