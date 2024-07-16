d9e58e026f6f4d8a581e7ac5c5edb242f324f055
Karsten Tausche <karsten@fairphone.com>
2024-07-16 16:40:57 +0200

    Revert "Merge tag 'LA.UM.9.8.c27-04100-SDM710.0' into int/13/fp3"
    This reverts commit d22b03e89250df5362844f971212fc5b6082a9eb, reversing
    changes made to 18e7fe74adb674fa62d37e32de86fc90a8ddb350.
    
    SDM710.0 tags are not meant for the 8953 platform. Drop the merge again
    to avoid potentially incompatible changes. Merge 89xx tags instead and
    cherry-pick other required patches separately.
    
    Issue: FP4-A14#304
    Change-Id: Ieb49152636fbcc192ecaf5e118b08f7d37444b23

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -426,8 +426,6 @@ struct fastrpc_file {
 	/* Identifies the device (MINOR_NUM_DEV / MINOR_NUM_SECURE_DEV) */
 	int dev_minor;
 	char *debug_buf;
-	/* Flag to indicate dynamic process creation status*/
-	bool in_process_create;
 };
 
 static struct fastrpc_apps gfa;
@@ -2293,15 +2291,6 @@ static int fastrpc_init_process(struct fastrpc_file *fl,
 			int siglen;
 		} inbuf;
 
-		spin_lock(&fl->hlock);
-		if (fl->in_process_create) {
-			err = -EALREADY;
-			pr_err("Already in create init process\n");
-			spin_unlock(&fl->hlock);
-			return err;
-		}
-		fl->in_process_create = true;
-		spin_unlock(&fl->hlock);
 		inbuf.pgid = fl->tgid;
 		inbuf.namelen = strlen(current->comm) + 1;
 		inbuf.filelen = init->filelen;
@@ -2494,11 +2483,6 @@ bail:
 		mutex_lock(&fl->fl_map_mutex);
 		fastrpc_mmap_free(file, 0);
 		mutex_unlock(&fl->fl_map_mutex);
-	}
-	if (init->flags == FASTRPC_INIT_CREATE) {
-		spin_lock(&fl->hlock);
-		fl->in_process_create = false;
-		spin_unlock(&fl->hlock);
 	}
 	return err;
 }
@@ -3268,7 +3252,6 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 	}
 	spin_lock(&fl->hlock);
 	fl->file_close = 1;
-	fl->in_process_create = false;
 	spin_unlock(&fl->hlock);
 	if (!IS_ERR_OR_NULL(fl->init_mem))
 		fastrpc_buf_free(fl->init_mem, 0);
@@ -3813,7 +3796,6 @@ static int fastrpc_device_open(struct inode *inode, struct file *filp)
 	fl->cid = -1;
 	fl->dev_minor = dev_minor;
 	fl->init_mem = NULL;
-	fl->in_process_create = false;
 	fl->qos_request = 0;
 	fl->refcount = 0;
 	filp->private_data = fl;
--- fs/file.c
+++ fs/file.c
@@ -748,9 +748,9 @@ static inline struct file *__fget_files_rcu(struct files_struct *files,
 }
 
 
-static struct file *__fget_files(struct files_struct *files, unsigned int fd,
-				 fmode_t mask, unsigned int refs)
+static struct file *__fget(unsigned int fd, fmode_t mask, unsigned int refs)
 {
+	struct files_struct *files = current->files;
 	struct file *file;
 
 	rcu_read_lock();
@@ -758,12 +758,6 @@ static struct file *__fget_files(struct files_struct *files, unsigned int fd,
 	rcu_read_unlock();
 
 	return file;
-}
-
-static inline struct file *__fget(unsigned int fd, fmode_t mask,
-				  unsigned int refs)
-{
-	return __fget_files(current->files, fd, mask, refs);
 }
 
 struct file *fget_many(unsigned int fd, unsigned int refs)
@@ -782,18 +776,6 @@ struct file *fget_raw(unsigned int fd)
 	return __fget(fd, 0, 1);
 }
 EXPORT_SYMBOL(fget_raw);
-
-struct file *fget_task(struct task_struct *task, unsigned int fd)
-{
-	struct file *file = NULL;
-
-	task_lock(task);
-	if (task->files)
-		file = __fget_files(task->files, fd, 0, 1);
-	task_unlock(task);
-
-	return file;
-}
 
 /*
  * Lightweight file lookup - no refcnt increment if fd table isn't shared.
@@ -817,7 +799,7 @@ static unsigned long __fget_light(unsigned int fd, fmode_t mask)
 	struct file *file;
 
 	if (atomic_read(&files->count) == 1) {
-		file = files_lookup_fd_raw(files, fd);
+		file = __fcheck_files(files, fd);
 		if (!file || unlikely(file->f_mode & mask))
 			return 0;
 		return (unsigned long)file;
